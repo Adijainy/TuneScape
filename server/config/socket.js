f0a901842cf0295a1a05d78977c3abd0b1f0c921
akashlitoriya <96015948+akashlitoriya@users.noreply.github.com>
2024-03-01 19:13:05 +0530

    Added the cors libary and removed unwanted socket events

--- server/config/socket.js
+++ server/config/socket.js
@@ -5,17 +5,7 @@
         socket.on("disconnect", () => {
             console.log("user disconnected");
         });
-        socket.on("addSongToQueue", (data) => {
-            console.log("song added to queue", data);
-
-            socket.broadcast.emit("receiveSong", data);
-            
-            
-        });
-        socket.on('songPlay', (data) => {
-            console.log('song is playing', data);
-            io.emit('playSong', data);
-        });
+        
     });
 }
 
