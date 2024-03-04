bfdbe3dc361fa76cbb1b0f05123b62087b154a06
akashlitoriya <96015948+akashlitoriya@users.noreply.github.com>
2024-03-04 18:21:55 +0530

    changed some code in lobby and user operation

--- src/services/apis/LobbyOperation.js
+++ src/services/apis/LobbyOperation.js
@@ -1,34 +1,37 @@
-import { Navigate } from "react-router";
 import { apiConnector } from "../apiConnector";
-
 import { lobbyEndpoints } from "../apis";
 import { createUser } from "./UserOperation";
+import { setUser } from "../../slices/userSlice";
 
-export async function createLobby(data, navigate) {
-  try {
-    const user = await createUser(data);
-    console.log("CREATE LOBBY", user);
-    navigate("/createLobby");
-    return user;
-  } catch (err) {
-    console.log(err);
-  }
+export function createLobby(data, navigate) {
+  return async (dispatch) => {
+    try {
+      const user = await createUser(data);
+      console.log("CREATE LOBBY", user);
+      dispatch(setUser(user));
+      navigate("/createLobby");
+    } catch (err) {
+      console.log(err);
+    }
+  };
 }
 
-export async function joinLobby(data, navigate) {
-  try {
-    const user = await createUser(data);
-    console.log("JOIN LOBBY", user);
-    data.userId = user._id;
-    const result = await apiConnector.post(
-      lobbyEndpoints.JOIN_LOBBY,
-      data,
-      null,
-      null
-    );
-    navigate("/lobby/" + result.data.code);
-    return result.data;
-  } catch (err) {
-    console.log(err);
-  }
+export function joinLobby(data, navigate) {
+  return async (dispatch) => {
+    try {
+      const user = await createUser(data);
+      console.log("JOIN LOBBY", user);
+      dispatch(setUser(user));
+      data.userId = user._id;
+      const result = await apiConnector.post(
+        lobbyEndpoints.JOIN_LOBBY,
+        data,
+        null,
+        null
+      );
+      navigate("/lobbyId:" + result.data._id);
+    } catch (err) {
+      console.log(err);
+    }
+  };
 }
